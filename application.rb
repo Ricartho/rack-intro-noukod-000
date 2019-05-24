09d0c1daa4bf1327cb1f0f5af68a9e7b7fd2e854
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-24 13:23:19 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is Ricartho FR"
+    resp.write "Hello, my name is Ricartho FRANCK"
     resp.finish
   end
 
